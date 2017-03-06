77190331c79a32531e99f7238f40edd9833d3317
Mick Holst <mick.holst@gmail.com>
2017-03-06 15:17:55 +0100

    - big commit

--- MiniExcercises/MiniEx_4/README.md
+++ MiniExcercises/MiniEx_4/README.md
@@ -1,2 +1,42 @@
-# miniex 3: Game of Life
+# miniex 4: Game of Life
 
+![Screenshot of the project](miniEx4_GameOfLife.png?raw=true "Screenshot of the project")
+
+I made an implementation of Conway's Game of Life, which is a zero-player game. This means that once the program is running there is no player, only calculation in the laws of the game. 
+
+The game has 2 states. The editor state and the playing state. In the editor, it's posible for the player to make the pattern of current generation in the Game of Life. The other state is playing, which, in timed intervals, evolves the pattern the player choose.
+
+You can switch from one state to the other, using the your mouse on the GUI. 
+
+
+A bit of the Game of Life:
+A cell can be either populated or unpopulated. White cells are unpopulated and black cells are populated. While in editor mode, you can click the cells to either populate or unpopulate them.
+
+Once the game is on, following rules are applied to each cell:
+* Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+* Any live cell with two or three live neighbours lives on to the next generation.
+* Any live cell with more than three live neighbours dies, as if by overpopulation.
+* Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+In the nature of the game, a generation can reach 3 states: dead, alive or repeating.
+Try playing around with the Game of Life, to see if you can reat a repeating generation, which creates an everlasting loop.
+
+There are multiple creations in the Game of Life, and you can check the Wikipedia to see some of these creations.
+[Wikipeadia: Conway's Game of Life](https://en.wikipedia.org/wiki/Conway's_Game_of_Life)
+
+## About the coding process
+I started of by making the cell structure, since it's the backbone of the project. Alot of the cell management, needs to be easy. Thus I made the creation of cells in a for-loop, nested in another for-loop. Creating each row, from left to right, in an order from top to bottom. This means that it's possible to find a cell by its index. 0; is the first cell (in the top to the left), 1; is the cell next to it etc. This is used when checking for the mouse position in the grid.
+Another indexation I made, was the indexX and indexY. This is for checking the neighbours of a cell. I could have calculated this from the previous mentioned indexation, but I didn't think of it in the process. There is room for optimazion here, instead of having to ways of indexing a cell and causing more memory to be used in the computer.
+
+After creating the indexation of cells and relations to it's neighbours, I made the user input in the editor-mode. This would come in handy when making the iteration of the game, since I would be able to test the game as it was final. Instead of having to script an instance and then test the game iteration.
+
+Finally I tweaked the game and the variables and added a GUI element, which made it eaiser for the user to interact with. Instead of having to write 'Press space to play'. 
+
+The code is a bit messy this time, because I had to figure out things as I went through it. I would consider going back and tweaking the code. 
+
+As last time, I'm making a to-do list. Mostly for myself, but feel free to play around with it aswel:
+* Making 'presets' to explain the game
+* Having a standard 'preset' entered when opening for the first time.
+* Making changes to the color scheme (unpopulated and populated cells) over time.
+* Making the player choose the time inbetween updates (maybe a slider).
+* Making the player set the grid size of the game.
--- MiniExcercises/MiniEx_4/sketch.js
+++ MiniExcercises/MiniEx_4/sketch.js
@@ -1,43 +1,89 @@
 class Cell {
-	constructor(curState, nextState, neighbourUp ,neighbourDown ,neighbourLeft ,neighbourRight ,indexX , indexY) {
+	constructor(curState, nextState, neighbourUp, neighbourDown, neighbourLeft, neighbourRight, neighbourUpperLeft, neighbourUpperRight, neighbourLowerLeft, neighbourLowerRight, indexX, indexY) {
 		this.curState = curState;
 		this.nextState = nextState;
 		this.neighbourUp = neighbourUp;
 		this.neighbourDown = neighbourDown;
 		this.neighbourLeft = neighbourLeft;
 		this.neighbourRight = neighbourRight;
+		this.neighbourUpperLeft = neighbourUpperLeft;
+		this.neighbourUpperRight = neighbourUpperRight;
+		this.neighbourLowerLeft = neighbourLowerLeft;
+		this.neighbourLowerRight = neighbourLowerRight;
 		this.indexX = indexX;
 		this.indexY = indexY;
 	}
 }
 
+class Button{
+	constructor(x,y,width,height,text)
+	{
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.text = text;
+	}
+}
+
+var buttonsEditor = [
+	new Button(10, 640, 125, 50, "Play"),
+	new Button(145, 640, 125, 50, "Clear")
+];
+
+var buttonsPlaying = [
+	new Button(10, 640, 125, 50, "Pause")
+];
+
 var cells = [];
 
-var gridCountX = 100;
-var gridCountY = 100;
+var gridCountX = 20;
+var gridCountY = 20;
+var gridSizeX = 800;
+var gridSizeY = 600;
+
+var isGameRunning = false;
+
+var timeBetweenGen = 200;
+var timer;
+
+var rows;
+var collums;
 
 function setup() 
 {
-	createCanvas(601,601);
+	createCanvas(801, 800);
+
+	timer = Date.now()
+
 	createCells();
+	drawCurrentGeneration();
 }
 
 function draw() 
 {
-	calculateNextGeneration();
-	drawCurrentGeneration();
+	GUI();
+
+	if(isGameRunning == true && timer <= Date.now())
+	{
+		calculateNextGeneration();
+		advanceGeneration();
+		drawCurrentGeneration();
+
+		timer = Date.now() + timeBetweenGen;
+	}	
 }
 
 function createCells()
 {
-	var rows = (width - 1) / gridCountX;
-	var collums = (height - 1) / gridCountY; 
+	rows = (gridSizeX) / gridCountX;
+	collums = (gridSizeY) / gridCountY; 
 
 	for(i = 0; i < collums; i++)
 	{
 		for(j = 0; j < rows; j++)
 		{
-			var tCell = new Cell();
+			var tCell = new Cell(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 			tCell.indexX = j;
 			tCell.indexY = i;
 			cells.push(tCell);
@@ -45,8 +91,6 @@ function createCells()
 	}
 
 	var cellsLength = cells.length;
-
-	console.log(cellsLength);
 
 	for(i = 0; i < cellsLength; i++)
 	{
@@ -58,6 +102,7 @@ function createCells()
 			var jIndexX = cells[j].indexX;
 			var jIndexY = cells[j].indexY;
 
+			//Create link between a cell and its neighbours
 			if(jIndexX == (iIndexX - 1) && jIndexY == iIndexY)
 				cells[i].neighbourLeft = cells[j];
 
@@ -70,6 +115,17 @@ function createCells()
 			if(jIndexY == (iIndexY + 1) && jIndexX == iIndexX)
 				cells[i].neighbourDown = cells[j];
 			
+			if(jIndexX == (iIndexX - 1) && jIndexY == (iIndexY - 1))
+				cells[i].neighbourUpperLeft = cells[j];
+
+			if(jIndexX == (iIndexX + 1) && jIndexY == (iIndexY - 1))
+				cells[i].neighbourUpperRight = cells[j];
+
+			if(jIndexX == (iIndexX - 1) && jIndexY == (iIndexY + 1))
+				cells[i].neighbourLowerLeft = cells[j];
+
+			if(jIndexX == (iIndexX + 1) && jIndexY == (iIndexY + 1))
+				cells[i].neighbourLowerRight = cells[j];
 		}
 	}
 }
@@ -82,19 +138,34 @@ function calculateNextGeneration()
 	{
 		var populatedNeighbours = 0;
 
-		if(cells[i].neighbourLeft != undefined && cells[i].neighbourLeft.curState === 1)
+		//Count amount of populated neighbours
+		if(cells[i].neighbourLeft != undefined && cells[i].neighbourLeft.curState == 1)
 			populatedNeighbours++;
 
-		if(cells[i].neighbourRight != undefined && cells[i].neighbourRight.curState === 1)
+		if(cells[i].neighbourRight != undefined && cells[i].neighbourRight.curState == 1)
 			populatedNeighbours++;
 
-		if(cells[i].neighbourUp != undefined && cells[i].neighbourUp.curState === 1)
+		if(cells[i].neighbourUp != undefined && cells[i].neighbourUp.curState == 1)
 			populatedNeighbours++;
 
-		if(cells[i].neighbourDown != undefined && cells[i].neighbourDown.curState === 1)
+		if(cells[i].neighbourDown != undefined && cells[i].neighbourDown.curState == 1)
 			populatedNeighbours++;
 
-		if(curState == 1)
+		if(cells[i].neighbourUpperLeft != undefined && cells[i].neighbourUpperLeft.curState == 1)
+			populatedNeighbours++;
+
+		if(cells[i].neighbourUpperRight != undefined && cells[i].neighbourUpperRight.curState == 1)
+			populatedNeighbours++;
+
+		if(cells[i].neighbourLowerLeft != undefined && cells[i].neighbourLowerLeft.curState == 1)
+			populatedNeighbours++;
+
+		if(cells[i].neighbourLowerRight != undefined && cells[i].neighbourLowerRight.curState == 1)
+			populatedNeighbours++;
+
+
+		//Apply the laws of Conway's Game of Life
+		if(cells[i].curState == 1)
 		{
 			if(populatedNeighbours < 2 || populatedNeighbours > 3)
 				cells[i].nextState = 0;
@@ -102,7 +173,7 @@ function calculateNextGeneration()
 			if(populatedNeighbours == 2 || populatedNeighbours == 3)
 				cells[i].nextState = 1;
 		}
-		else if(curState == 0)
+		else if(cells[i].curState == 0)
 		{
 			if(populatedNeighbours == 3)
 				cells[i].nextState = 1;
@@ -133,21 +204,182 @@ function drawCurrentGeneration()
 		}
 		else if(cells[i].curState == 0)
 		{
-			noFill();
+			fill(255);
 			rect(cells[i].indexX * gridCountX, cells[i].indexY * gridCountY, gridCountY, gridCountX);
+		}
+	}
+}
+
+function wipeGeneration()
+{
+	var cellsLength = cells.length;
+
+	for(var i = 0; i < cellsLength; i++)
+	{
+		cells[i].curState = 0;
+		cells[i].nextState = 0;
+	}
+}
+
+function wipeNextGeneration()
+{
+	var cellsLength = cells.length;
+
+	for(var i = 0; i < cellsLength; i++)
+	{
+		cells[i].nextState = 0;
+	}
+}
+
+function editorMode()
+{
+	var mouseInGrid = true;
+	var mouseGridX = Math.ceil(mouseX / gridCountX) - 1;
+	var mouseGridY = Math.ceil(mouseY / gridCountX) - 1;
+	var cellIndex;
+
+	if(mouseX == 0)
+		mouseGridX = 0;
+	if(mouseY == 0)
+		mouseGridY = 0;
+
+	if(mouseX > gridSizeX)
+		mouseInGrid = false
+	if(mouseY > gridSizeY)
+		mouseInGrid = false;
+
+	if(mouseInGrid == true)
+	{
+		cellIndex = (mouseGridY * (rows)) + mouseGridX;
+
+		if(cells[cellIndex].curState == 0)
+		{
+			cells[cellIndex].curState = 1;
+			fill(0);
+			rect(cells[cellIndex].indexX * gridCountX, cells[cellIndex].indexY * gridCountY, gridCountY, gridCountX);
+		}
+		else if(cells[cellIndex].curState == 1)
+		{
+			cells[cellIndex].curState = 0;
+			fill(255);
+			rect(cells[cellIndex].indexX * gridCountX, cells[cellIndex].indexY * gridCountY, gridCountY, gridCountX);
 		}
 	}
 }
 
 function mouseClicked()
 {
-	
+	if(isGameRunning == false)
+	{
+		editorMode();
+		checkEditorButtons();
+	}
+	else
+	{
+		checkPlayingButtons();
+	}
+
 }
 
-
-/* Draw grid: 
-for(i = 0; i < cells.length; i++)
+function GUI()
+{
+	if(isGameRunning == false)
 	{
-		rect(cells[i].indexX * gridCountX, cells[i].indexY * gridCountY, gridCountX, gridCountY);
+		var buttonsLength = buttonsEditor.length;
+
+		for(var i = 0; i < buttonsLength; i++)
+		{
+			fill(200);
+			rect(buttonsEditor[i].x, buttonsEditor[i].y, buttonsEditor[i].width, buttonsEditor[i].height);
+
+			fill(0);
+			textAlign(CENTER);
+			textSize(20);
+			text(buttonsEditor[i].text, buttonsEditor[i].x + (buttonsEditor[i].width * 0.5), buttonsEditor[i].y + (buttonsEditor[i].height * 0.5) + 5)
+		}
 	}
-*/
+	else
+	{
+		var buttonsLength = buttonsPlaying.length;
+
+		for(var i = 0; i < buttonsLength; i++)
+		{
+			fill(200);
+			rect(buttonsPlaying[i].x, buttonsPlaying[i].y, buttonsPlaying[i].width, buttonsPlaying[i].height);
+
+			fill(0);
+			textAlign(CENTER);
+			textSize(20);
+			text(buttonsPlaying[i].text, buttonsPlaying[i].x + (buttonsPlaying[i].width * 0.5), buttonsPlaying[i].y + (buttonsPlaying[i].height * 0.5) + 5)
+		}
+
+
+	}
+}
+
+function checkEditorButtons()
+{
+	var buttonText;
+	var buttonsLength = buttonsEditor.length;
+
+	for(var i = 0; i < buttonsLength; i++)
+	{
+		if(mouseX > buttonsEditor[i].x && mouseX < (buttonsEditor[i].x + buttonsEditor[i].width))
+		{
+			if(mouseY > buttonsEditor[i].y && mouseY < (buttonsEditor[i].y + buttonsEditor[i].height))
+			{
+				buttonText = buttonsEditor[i].text;
+				break;
+			}
+		}
+	}
+
+	if(buttonText != undefined)
+	{
+		switch(buttonText)
+		{
+			case "Play":
+				isGameRunning = true;
+				clear();
+				break;
+
+			case "Clear":
+				wipeGeneration();
+				drawCurrentGeneration();
+				break;
+
+			default:
+
+		}
+	}
+}
+
+function checkPlayingButtons()
+{
+	var buttonIndex;
+	var buttonsLength = buttonsPlaying.length;
+
+	for(var i = 0; i < buttonsLength; i++)
+	{
+		if(mouseX > buttonsPlaying[i].x && mouseX < (buttonsPlaying[i].x + buttonsPlaying[i].width))
+		{
+			if(mouseY > buttonsPlaying[i].y && mouseY < (buttonsPlaying[i].y + buttonsPlaying[i].height))
+			{
+				buttonText = buttonsPlaying[i].text;
+			}
+		}
+	}
+
+	if(buttonText != undefined)
+	{
+		switch(buttonText)
+		{
+			case "Pause":
+				isGameRunning = false;
+				wipeGeneration();
+				break;
+			
+			default:
+		}
+	}
+}
