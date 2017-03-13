6f2ef122d5c3d00ec6a169027d9d85f0b55ff39e
Mick Holst <mick.holst@gmail.com>
2017-03-13 02:34:08 +0100

    - upload of miniex 5

---
+++ MiniExcercises/MiniEx_5/README.md
@@ -0,0 +1,13 @@
+# miniex 5: Bad poem generator
+
+![Screenshot of the project](miniEx5_BadPoemGenerator.png?raw=true "Screenshot of the project")
+
+For this miniex, I made a simple little generator of poems. I tried approaching the project, with making a generator which made versions of the classic "roses are red"-poems, but failed. Instead I made somewhat of a mess, which you can try out [here](https://epsilon99.github.io/AestheticProgrammingCourse/MiniExcercises/MiniEx_2/).
+
+I really didn't like this miniexcercise too much. The libary we were to use, didn't really have an easy access to it, plus some features really didn't make much sense to me (I'll get back to those). It might help getting to know the libary a bit better, but I really didn't find its structure and functions intuitive enough for me to find a proper workflow.
+
+**Some of the problems I had with the Libary.**
+As mentioned, I tried to use a certain poem as the structure of the generator. But trying to randomize something with the framework of RiTa, seemed to much of a hazzle. For example, you can easily find the POS when you're searching for a random word, but trying to find the syllabal count is near impossible with the libary (Of what I found searching through the refference). 
+   This also was apparent when finding words that rhymed. I could easily get a whole array of words that matched the criteria of the rhyming par, but nothing else; no POS no syllabal count etc. This means that you have to process the entire rhyme-array yourself, instead of just having the libary sort these thing with at parsing of parameters.
+
+These things kinda annoyed me, because the reason of having the libary in the first place, is to make it easier to use words and proper grammar. But if I have to do most of the work myself, with limitations of the RiTa libary, then it maybe hinders more than it helps. As a creative idea, I like the RiTa libary. But in practice, I do not quite fancy it.
--- MiniExcercises/MiniEx_5/sketch.js
+++ MiniExcercises/MiniEx_5/sketch.js
@@ -1,7 +1,110 @@
-function setup() {
+var lexicon;
 
+var poem;
+var tRandomPoem = [];
+
+function preload()
+{
+	poem = loadStrings("data/poem.txt");
 }
 
-function draw() {
+function setup()
+{
+	createCanvas(windowWidth,windowHeight);
+	lexicon = RiLexicon();
+	processPoem();
 
+	var syll = RiTa.getSyllables("derp");
+	console.log(syll);
+}
+
+function draw()
+{
+	createCanvas(windowWidth,windowHeight);
+	clear();
+	writeText();
+}
+
+function processPoem()
+{
+	var wordToRhyme1;
+	var wordToRhyme2;
+
+	for(var i = 0; i < poem.length; i++)
+	{
+		var r = new RiString(poem[i]);
+		var words = r.words();
+		var rPos = r.pos();
+		var randomVerse = "";
+
+		// Messy shit, I should just have allocated the whole thing as ritastrings, bur didn't think of it in time.
+		// Therefore I need to do some dumb coding to get the words I wanna rhyme on.
+		
+		for(j = 0; j < words.length; j++)
+		{
+			if(i == poem.length - 2 && j == words.length -1)
+			{
+				var tRhyme = lexicon.rhymes(wordToRhyme1);
+
+				if(tRhyme.length > 0)
+				{
+					randomVerse += tRhyme[0];
+				}
+			}
+			else if(i == poem.length - 1 && j == words.length -1)
+			{
+				var tRhyme = lexicon.rhymes(wordToRhyme2);
+				
+				if(tRhyme.length > 0)
+				{
+					randomVerse += tRhyme[0];
+				}
+			}
+			else
+			{
+				var randomWord = lexicon.randomWord(rPos[j], 2);
+
+				if(i == 0 && j == words.length - 1)
+				{
+					wordToRhyme1 = randomWord;
+				}
+				if(i == 1)
+				{
+					wordToRhyme2 = randomWord;
+				}
+
+				randomVerse += randomWord;
+			}
+	
+			randomVerse += " ";
+		}
+
+		tRandomPoem[i] = randomVerse;
+	}
+}
+
+function writeText()
+{
+	var curTextSize = windowHeight / 15;
+
+	for(var i = 0; i < tRandomPoem.length; i++)
+	{
+		fill(0);
+		textAlign(CENTER);
+		textSize(curTextSize);
+		text(tRandomPoem[i], width * 0.5, curTextSize + (curTextSize * i) + (curTextSize) * 0.5);
+	}
+
+	fill(0);
+	textAlign(CENTER);
+	textStyle(ITALIC);
+	textSize(curTextSize / 2);
+	text("click the mouse \n to get a new meaningful poem!", width * 0.5, curTextSize + (curTextSize * 6) + (curTextSize) * 0.5);
+}
+
+function mouseClicked()
+{
+	clear();
+	processPoem();
+	writeText();
 }
